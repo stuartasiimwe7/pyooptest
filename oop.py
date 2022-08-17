75691f299e55f1bf701155c865fb4c99d8eb40e7
Stuart Asiimwe <stuartgabriel@ymail.com>
2022-08-18 08:44:46 +0900

    more to human attribute

--- oop.py
+++ oop.py
@@ -1,12 +1,16 @@
 class human:
-    def man(self,name):
-        print("I am the Man")
-        return name
-        
-    def woman(self,name):
-        print("I am the Woman")
-        return name
+    def __init__(self, name, age, height) -> None:
+        self.name = name
+        self.age = age
+        self.height = height
+        print(name,age,height)
 
-human1 = human()
-human1.man("Stuart")
-print(human1)
+    def man(self):
+        print("I am the Man")
+         
+    def woman(self):
+        print("I am the Woman")
+
+human1 = human("Stuart",25,182)
+#human1.man("Stuart",25,182)
+#print(human1)
