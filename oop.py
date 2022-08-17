d3cd2c89bf07f50c2f53e479b7365f24cd677207
Stuart Asiimwe <stuartgabriel@ymail.com>
2022-08-18 05:05:45 +0900

    After Class Build

---
+++ oop.py
@@ -0,0 +1,9 @@
+class human:
+    def man(name, age, height):
+        def _int_(self):
+            print("I am the Man")
+        
+    def woman(name, age, height):
+        def _init_(self):
+            print("I am the Woman")
+
