d39bf94330db3a8b73afb2fce432e50192b35801
Stuart Asiimwe <stuartgabriel@ymail.com>
2024-12-01 07:31:08 +0900

    area_done

--- oop3.py
+++ oop3.py
@@ -1,12 +1,18 @@
-name = "stuart"
-weight = 76.5
+#name = "stuart"
+#weight = 76.5
 
 #print(type(name))
 
-#typecssting
+#typecssting 0178
 #weight = str(weight)
 #print(type(weight))
 
 #print(name)
 #age = int(input("How old are you?\n"))
 #print(f"oh hi {name}!In South Korea you would be {age+1}")
+
+#area of a rectangle
+length = float(input("What is the Length?\n"))
+width = float(input("and what is the Width?\n"))
+area = length * width
+print(f"the area of that rectangle is {area}cm squared")
