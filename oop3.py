bd9ce2d995df791300a440099ab48c99c2e07351
Stuart Asiimwe <stuartgabriel@ymail.com>
2024-12-01 07:22:25 +0900

    next

--- oop3.py
+++ oop3.py
@@ -1,12 +1,12 @@
 name = "stuart"
 weight = 76.5
 
-print(type(name))
+#print(type(name))
 
 #typecssting
-weight = str(weight)
-print(type(weight))
+#weight = str(weight)
+#print(type(weight))
 
-print(name)
-age = int(input("How old are you?\n"))
-print(f"oh hi {name}!In South Korea you would be {age+1}")
+#print(name)
+#age = int(input("How old are you?\n"))
+#print(f"oh hi {name}!In South Korea you would be {age+1}")
